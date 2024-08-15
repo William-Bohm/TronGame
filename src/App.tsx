12f0ffc8cb397f76d795daf848d5566ea4bad0d8
Slater Bohm <slaterbohm@Slaters-MacBook-Pro.local>
2024-08-15 16:39:52 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <h1>Tron game or nah playah?</h1>
+    );
+};
+
+export default App;
---
+++ webpack.config.mjs
@@ -0,0 +1,38 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
