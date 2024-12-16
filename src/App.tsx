2b7e8146bec954f0feafc67573e0c1ad7f5657da
Slater Bohm <slaterbohm@Slaters-MacBook-Pro-2.local>
2024-12-16 11:49:22 -0500

    add url routing for ui dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TronGame from "./TronGame";
+import MainMenu from "./components/mainMenu/MainMenu";
 
 const App: React.FC = () => {
   return (
-      <div>
-        <TronGame/>
-      </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<TronGame />} />
+          <Route path="/game" element={<MainMenu />} />
+        </Routes>
+    </Router>
   );
 };
 
---
+++ src/components/mainMenu/MainMenu.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import NeonLogo from "../NeonLogo";
+
+const MainMenu: React.FC = () => {
+    return (
+        <div>
+            <NeonLogo/>
+        </div>
+    )
+};
+
+export default MainMenu;
