9e63a6545c0bd6024c2fef83966a084501ccda63
Slater Bohm <slaterbohm@Slaters-MacBook-Pro-3.local>
2025-01-21 09:08:48 -0500

    update

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -1,99 +1,100 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import TerserPlugin from 'terser-webpack-plugin';
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  mode: 'production',
-  entry: './src/index.tsx',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
-    publicPath: '/'
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-    splitChunks: {
-      chunks: 'all',
-      maxInitialRequests: Infinity,
-      minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-            return `vendor.${packageName.replace('@', '')}`;
-          },
+    mode: 'production',
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        chunkFilename: '[name].[contenthash].js',
+        publicPath: '/'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()],
+        splitChunks: {
+            chunks: 'all',
+            maxInitialRequests: Infinity,
+            minSize: 0,
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name(module) {
+                        const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                        return `vendor.${packageName.replace('@', '')}`;
+                    },
+                },
+            },
         },
-      },
     },
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    fallback: {
-      fs: false,
-      path: false,
-      crypto: false,
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        fallback: {
+            fs: false,
+            path: false,
+            crypto: false,
+        },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                {from: 'public', to: ''},
+            ],
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static',
+            openAnalyzer: false,
+        }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-    }),
-    new CopyPlugin({
-      patterns: [
-        { from: 'public', to: '' },
-      ],
-    }),
-    new BundleAnalyzerPlugin({
-      analyzerMode: 'static',
-      openAnalyzer: false,
-    }),
-  ],
-  devServer: {
-    static: [
-      {
-        directory: path.join(__dirname, 'dist'),
-      },
-      {
-        directory: path.join(__dirname, 'public'),
-      },
-    ],
-    compress: true,
-    port: 3000,
-    historyApiFallback: true,
-  },
-  ignoreWarnings: [/Failed to parse source map/],
-  externals: {
-    'onnxruntime-node': 'onnxruntime-node'
-  },
+    devServer: {
+        static: [
+            {
+                directory: path.join(__dirname, 'dist'),
+            },
+            {
+                directory: path.join(__dirname, 'public'),
+            },
+        ],
+        compress: true,
+        port: 3000,
+        historyApiFallback: true,
+    },
+    ignoreWarnings: [/Failed to parse source map/],
+    externals: {
+        'onnxruntime-node': 'onnxruntime-node'
+    },
 };
